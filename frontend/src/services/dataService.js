c4395c3f871b4eb612d182c2afbdee6960b48aff
muikku <markus.muinonen@gmail.com>
2019-02-26 12:08:55 +0200

    lala

--- frontend/src/services/dataService.js
+++ frontend/src/services/dataService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = '/api/unzipAndParse'
+const baseUrl = 'https://whispering-castle-37289.herokuapp.com/api/unzipAndParse'
 
 const getData = async (givenUrl) => {
   const request = await axios.post(baseUrl, {
